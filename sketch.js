05036e8efaa47dadbcbdbf1a6fc98ec66d7ea1ed
Eblake-7377 <83600906+Eblake-7377@users.noreply.github.com>
2021-05-05 19:48:25 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -19,8 +19,6 @@ let r = 0
 let ff = false;
 
 
-function preload() {}
-
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
@@ -55,15 +53,13 @@ function setup() {
   maxD = dist(0, 0, 200, 200);
 
 }
-
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 function draw() {
   background(0);
-
   push();
   directionalLight(255, 255, 255, 0, -1, 0);
   translate(0, 200, -300);
   rotateX(-QUARTER_PI);
-
   let offset = 0;
   for (let z = 0; z < height; z += core) {
 
@@ -72,13 +68,13 @@ function draw() {
       let d = dist(x, z, width / 2, height / 2);
       let offset = map(d, 0, maxD, -PI, PI);
       let a = angle + offset;
-      let h = floor(map(sin(a), -1, 1, 100, 200));
+      let h = floor(map(sin(a), -1, 1, 100, 480));
       translate(x - width / 2, 0, z - height / 2);
-      // normalMaterial(10);
+      ambientMaterial(255);
       // stroke('MidnightBlue');
       stroke(random(20, 155), random(20, 155), random(20, 155));
       fill(0);
-      box(core, h, core);
+      box(core - 2, h, core - 2);
 
       // rect(x- width/2 + w/2, 0, w-2, h);
       pop();
@@ -89,6 +85,20 @@ function draw() {
   angle -= 0.075;
   pop();
 
+  push();
+  translate(0, 80, -200);
+
+  rotateX(-2.26893);
+  rotateZ(0.785398);
+
+  stroke('#5500ff');
+  fill(0);
+
+  torus(width * 0.9, 160, 24, 4);
+  pop();
+
+  push();
+  translate(0, -100, 0);
   ////the continuous rotation of puzzle
   // p += 0.005;
   // rotateX(p);
@@ -100,41 +110,134 @@ function draw() {
   // check the keyboard
   handleKeyboard();
 
+
   // draw the puzzle piece
-  //core puzzle
+  fill(0);
+  stroke('#4d804d');
   CU1.display();
-  CD1.display();
-  //outer layer
   CU2.display();
-  Puzz1.display();
-  Puzz2.display();
-  CD2.display();
   CU3.display();
-  Puzz3.display();
+  CU4.display();
+  CU5.display();
+
+  fill(0);
+  stroke('Peru');
+  CD1.display();
+  CD2.display();
   CD3.display();
   CD4.display();
-  CU4.display();
   CD5.display();
-  CU5.display();
+
+  fill(0);
+  stroke('#2e50b8');
+  Puzz1.display();
+  Puzz2.display();
+  Puzz3.display();
   Puzz7.display();
+
+  noFill();
+  stroke(255);
   drop.display();
   drop1.display();
 
   if (ff == true) {
+    background(255);
+
     drop.move(0, 3, 0);
     drop1.move(0, 3, 0);
+    fill('SpringGreen');
+    stroke('LimeGreen');
+    CU1.display();
+    CU2.display();
+    CU3.display();
+    CU4.display();
+    CU5.display();
 
-    if (drop.y > 500) {
+    fill('GoldenRod');
+    stroke('Peru');
+    CD1.display();
+    CD2.display();
+    CD3.display();
+    CD4.display();
+    CD5.display();
+
+    fill('DodgerBlue');
+    stroke('RoyalBlue');
+    Puzz1.display();
+    Puzz2.display();
+    Puzz3.display();
+    Puzz7.display();
+
+    fill('magenta');
+
+    stroke(0);
+    drop1.display();
+    fill(255, 51, 153, 150);
+
+    drop.display();
+    pop();
+
+    push();
+    directionalLight(255, 255, 255, 0, -1, 0);
+    translate(0, 200, -300);
+    rotateX(-QUARTER_PI);
+
+    let offset = 0;
+    for (let z = 0; z < height; z += core) {
+
+      for (let x = 0; x < width; x += core) {
+        push();
+        let d = dist(x, z, width / 2, height / 2);
+        let offset = map(d, 0, maxD, -PI, PI);
+        let a = angle + offset;
+        let h = floor(map(sin(a), -1, 1, 100, 505));
+        translate(x - width / 2, 0, z - height / 2);
+        normalMaterial(10);
+        // stroke('MidnightBlue');
+        stroke(random(20, 155), random(20, 155), random(20, 155));
+        // fill(255);
+        box(core, h, core);
+
+        // rect(x- width/2 + w/2, 0, w-2, h);
+        pop();
+      }
+      offset += 0.1;
+
+    }
+    angle -= 0.075;
+    pop();
+
+    if (drop.y > 600) {
       ff = false;
-        background(255);
-      
+      CU1 = new puzzleCUp(-core / 2, 0, 0, core);
+      CD1 = new puzzleCDown(-core / 2, -core, core, core);
 
+      //2nd layer
+      CU2 = new puzzleCUp(core / 2, core, -core, core);
+      Puzz1 = new puzzlePiece(core * 1.5, -core, -core, core);
+      Puzz2 = new puzzlePiece(core * 1.5, -core * 2, 0, core);
+      CD2 = new puzzleCDown(-core * 1.5, -core * 2, core * 2, core);
+      CU3 = new puzzleCUp(-core * 1.5, core, -core, core);
+      Puzz3 = new puzzlePiece(-core * 1.5, -core, -core, core);
+      CD3 = new puzzleCDown(-core * 1.5, 0, 0, core);
+      CD4 = new puzzleCDown(-core * 1.5, -core, core, core);
+      CU4 = new puzzleCUp(core / 2, core, core, core);
+      CD5 = new puzzleCDown(-core / 2, -core, core * 2, core);
+      CU5 = new puzzleCUp(-core * 1.5, core, core, core);
+      Puzz4 = new puzzlePiece(-core * 1.5, core, core * 2, core);
+      Puzz5 = new puzzlePiece(-core * 1.5, 0, core * 2, core);
+      CD6 = new puzzleCDown(core / 2, 0, core * 2, core);
+      CD7 = new puzzleCDown(core / 2, -core * 2, core, core);
+      Puzz6 = new puzzlePiece(-core * 1.5, 0, 0, core);
+      Puzz7 = new puzzlePiece(-core * 1.5, core, core * 2, core);
+
+      //core/center on puzzle
+      drop = new Drop(0, -core / 2, core / 2, core);
+      drop1 = new Drop(0, -core / 2, core / 2, core / 2);
     }
 
 
   }
-
-
 }
 
 class puzzlePiece {
@@ -154,8 +257,9 @@ class puzzlePiece {
   }
 
   display() {
-    noFill();
-    stroke('#2e50b8');
+    // noFill();
+    // fill(0);
+    // stroke('#2e50b8');
     push();
     translate(this.x, this.y, this.z);
     box(this.size);
@@ -184,9 +288,10 @@ class puzzleCUp {
 
   display() {
     // texture(w);
-    noFill();
+    // noFill();
     // stroke('BlanchedAlmond');
-    stroke('#4d804d');
+    // fill(0);
+    // stroke('#4d804d');
     push();
     //corner left edge form voewers perspective
     translate(this.x, this.y, this.z);
@@ -233,8 +338,8 @@ class puzzleCDown {
 
   display() {
     // texture(w);
-    noFill();
-    stroke('Peru');
+    // stroke('Peru');
+    // fill(0);
     // stroke('MediumSlateBlue');
     push();
     //corner left edge form voewers perspective
@@ -274,9 +379,8 @@ class Drop {
   }
 
   display() {
-    // texture(w);
-    noFill();
-    stroke(255);
+    // noFill();
+    // stroke(255);
     push();
     translate(this.x, this.y, this.z);
     // texture(w);
@@ -285,6 +389,9 @@ class Drop {
   }
 }
 
+function Again() {
+
+}
 
 function handleKeyboard() {
 
@@ -296,53 +403,46 @@ function handleKeyboard() {
       CD5.move(0, 0, 10);
     } else if (key == 'b') {
       CD6.move(0, 0, 10);
-      CU4.move(0, 10, 0);
+      CU4.move(0, -10, 0);
 
     } else if (key == 'c') {
       Puzz5.move(-10, 0, 10);
       CD4.move(0, 0, 10);
 
     } else if (key == 'd') {
-      Puzz4.move(0, 10, 0);
+      Puzz4.move(0, -10, 0);
       CD3.move(-10, 0, 0);
     } else if (key == 'e') {
-      CU5.move(0, 10, 0);
+      CU5.move(0, -10, 0);
       Puzz7.move(0, -10, 0);
     } else if (key == 'f') {
       Puzz3.move(-10, 0, -10);
-      CU3.move(0, 10, 0);
+      CU3.move(0, -10, 0);
     } else if (key == 'g') {
 
-    } else if (key == 'h') {
+    } else if (key == 'g') {
       CD2.move(0, -10, 0);
-    } else if (key == 'i') {
+    } else if (key == 'h') {
       Puzz2.move(0, -10, 0);
-    } else if (key == 'j') {
+    } else if (key == 'i') {
       Puzz1.move(10, 0, 0);
+    } else if (key == 'j') {
+      CU2.move(10, -10, 0);
     } else if (key == 'k') {
-      CU2.move(10, 10, 0);
-    } else if (key == 'l') {
       CD1.move(0, -10, 0);
-    } else if (key == 'm') {
-      CU1.move(0, 10, 0);
-    } else if (key == ENTER) {
-      // this.size=v
-      //   v++;
+    } else if (key == 'l') {
+      CU1.move(0, -10, 0);
+    } else if (keyCode == ENTER) {
+      ff = false;
     } else if (keyCode == BACKSPACE) {
-      // drop1.display(false);
-      // if (ff === true) {
-      //   drop.move(this.x, fall, this.z);
-      //   drop1.move(this.x, fall, this.z);
-      //   fall++;
-      //   if (fall > height) {
-      //     background(random(255));
-      //     stroke(random(255), random(255), random(255));
-      //   }
-      // }
       ff = true;
     }
   }
 }
 
 
-//reference from https://www.youtube.com/watch?v=IKB1hWWedMk (Terrian Generation with Perlin)
+
+
+
+
+//reference from https://www.youtube.com/watch?v=H81Tdrmz2LA (Terrian Generation - Coding Challenge )
